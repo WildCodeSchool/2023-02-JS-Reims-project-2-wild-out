3ba0411e5720ba9a5deb66dfecaf9ca5cd9e191c
zeitounmax <mthiry1@gmail.com>
2023-04-12 12:00:56 +0200

    test_api_v1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import "./App.css";
 
 function App() {
+  fetch("http://localhost:5000/outing")
+    .then((resp) => resp.json())
+    .then((data) => console.info(data));
   return (
     <div className="App">
       <Home />
