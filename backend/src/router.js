f6ad132bd5b955632f11399f77a6464a8e5135a9
Elodie0530 <106558856+Elodie0530@users.noreply.github.com>
2023-04-12 16:59:09 +0200

    Update router.js
    Retirer le tableau des voitures de l'exercice du 12 avril utiliser afin de faire communiquer le backend et le frontend.

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,13 +4,6 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
-const list = [
-  { id: 1, name: "Bmw" },
-  { id: 2, name: "Renault" },
-  { id: 3, name: "Audi" },
-  { id: 4, name: "Peugeot" },
-];
-
 router.get("/event-listings", (req, res) => {
   res.json(list);
 });
