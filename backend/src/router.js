3f055c54c70963574b5554c107ad0f0cd3aa08c9
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-04-12 15:44:34 +0200

    Affichage de l'heure et de la température 01

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,17 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+const list = [
+  { id: 1, name: "Bmw" },
+  { id: 2, name: "Renault" },
+  { id: 3, name: "Audi" },
+  { id: 4, name: "Peugeot" },
+];
+
+router.get("/event-listings", (req, res) => {
+  res.json(list);
+});
+
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,34 @@
 import Home from "./pages/Home";
-
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState(null);
+
+  const fetchOneTime = () => {
+    fetch(
+      "https://api.open-meteo.com/v1/forecast?latitude=49.27&longitude=4.03&hourly=temperature_2m"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setFetchedData(data);
+      });
+  };
+
   return (
     <div className="App">
       <Home />
       <p>coucou</p>
+
+      <button type="button" onClick={fetchOneTime}>
+        click me
+      </button>
+      {fetchedData &&
+        fetchedData.hourly.time.map((hour, index) => (
+          <p key={hour}>
+            heure :{hour} temp:{fetchedData.hourly.temperature_2m[index]}
+          </p>
+        ))}
     </div>
   );
 }
