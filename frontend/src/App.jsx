c9bb495f1ca6d173a60920de3d1241b1dd97fe02
zeitounmax <mthiry1@gmail.com>
2023-04-12 17:41:01 +0200

    add-cssStyle-Map-MaxThiry

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
-import Home from "./pages/Home";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 import "./App.css";
 
 function App() {
-  fetch("http://localhost:5000/outing")
+  /* fetch("http://localhost:5000/outing")
     .then((resp) => resp.json())
-    .then((data) => console.info(data));
+    .then((data) => console.info(data)); */
   return (
     <div className="App">
-      <Home />
-      <p>coucou</p>
+      <MapContainer
+        center={[49.256948, 4.019683, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={[49.256948, 4.019683, -0.09]}>
+          <Popup>
+            <h3>Wild Code School</h3>. <br />{" "}
+            <h4> 5 rue saint Brice 51000 Reims</h4>
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
