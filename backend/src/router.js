a82fe013adf2253ee0bfa45079144bf79d6d6589
Elodie0530 <uneondine@gmail.com>
2023-04-12 16:30:04 +0200

    exercice mise en relation des données entre backend et frontend

--- backend/src/router.js
+++ backend/src/router.js
@@ -4,6 +4,17 @@ const router = express.Router();
 
 const itemControllers = require("./controllers/itemControllers");
 
+const exitIdea = [
+  { id: 1, exit: "beach" },
+  { id: 2, exit: "opera" },
+  { id: 3, exit: "ballad" },
+  { id: 4, exit: "movie theater" },
+];
+
+router.get("/exitIdea", (req, res) => {
+  res.json(exitIdea);
+});
+
 router.get("/items", itemControllers.browse);
 router.get("/items/:id", itemControllers.read);
 router.put("/items/:id", itemControllers.edit);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,30 @@
+import { useState } from "react";
 import Home from "./pages/Home";
 
 import "./App.css";
 
 function App() {
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const fetchOneTime = () => {
+    fetch("http://localhost:5000/exitIdea")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setFetchedData(data);
+      });
+  };
+
   return (
     <div className="App">
       <Home />
       <p>coucou</p>
+      <button type="button" onClick={fetchOneTime}>
+        Click here
+      </button>
+
+      {fetchedData.map((exitIdea) => (
+        <p key={exitIdea.id}>{exitIdea.exit}</p>
+      ))}
     </div>
   );
 }
