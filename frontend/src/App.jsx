955a3b44a2ea98325be0b29fcff0f26a1f0231ee
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-04-13 18:55:53 +0200

    Changement du code qui permet de mettre à jour la date dans n'importe quels formats

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import { useState } from "react";
 import "./App.css";
+import Home from "./pages/Home";
+import sunImage from "./assets/sun.png";
+import cloudImage from "./assets/cloud.png";
+import rainImage from "./assets/rain.png";
 
 function App() {
   const [fetchedData, setFetchedData] = useState(null);
+  const [todaysData, setTodaysData] = useState([]);
 
   const fetchOneTime = () => {
     fetch(
@@ -11,24 +16,62 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         setFetchedData(data);
+        // extract today's date and filter the data for the same day as today
+        const now = new Date();
+
+        const dateStrings = data.hourly.time;
+
+        const dates = dateStrings.map((dateString) => new Date(dateString));
+
+        const filteredDates = dates.filter(
+          (date) =>
+            date.getFullYear() === now.getFullYear() &&
+            date.getMonth() === now.getMonth() &&
+            date.getDate() === now.getDate()
+        );
+        setTodaysData(filteredDates);
       });
+  };
+
+  const getConditionImage = (temperature) => {
+    if (temperature > 15) {
+      return sunImage;
+    }
+    if (temperature > 10) {
+      return cloudImage;
+    }
+    return rainImage;
   };
 
   return (
     <div className="App">
-      <br />
+      <Home />
+      <p>coucou</p>
       <button type="button" onClick={fetchOneTime}>
-        Cliquer ici pour afficher la date,l'heure et la météo
+        click me
       </button>
-
-      {fetchedData &&
-        fetchedData.hourly.time.map((hour, index) => (
-          <p key={hour}>
-            Date : {hour.substr(0, 10)} <br />
-            Heure : {hour.substr(11, 5)} <br />
-            temp : {fetchedData.hourly.temperature_2m[index]} °C
-          </p>
-        ))}
+      {todaysData.map((date, index) => (
+        <p
+          key={date.getTime()}
+          style={{
+            color: `${date < new Date() ? "red" : "green"}`,
+          }}
+        >
+          Date :{" "}
+          {`${date.getDate()}/${(date.getMonth() + 1)
+            .toString()
+            .padStart(2, "0")}/${date.getFullYear()}`}
+          <br />
+          Heure : {`${date.getHours()}h`}
+          <br />
+          Température: {fetchedData.hourly.temperature_2m[index]} °C
+          <img
+            src={getConditionImage(fetchedData.hourly.temperature_2m[index])}
+            alt="condition"
+            width="30px"
+          />
+        </p>
+      ))}
     </div>
   );
 }
