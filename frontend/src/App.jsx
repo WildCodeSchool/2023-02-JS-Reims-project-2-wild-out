db09f9f10e45c6ec6a335ae54bcba7aa23c078e1
zeitounmax <mthiry1@gmail.com>
2023-04-13 15:07:44 +0200

    modify-maps

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,7 @@
 import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
-import Home from "./pages/Home";
-
 import "./App.css";
 
 function App() {
-  fetch("http://localhost:5000/outing")
-    .then((resp) => resp.json())
-    .then((data) => console.info(data));
   return (
     <div className="App">
       <MapContainer
