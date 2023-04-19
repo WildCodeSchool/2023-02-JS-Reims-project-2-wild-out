6809b1809cc7c9cc47a298df29d863344f33027d
AHMED EL AMRI <ahmed-elamri@outlook.fr>
2023-04-19 12:10:32 +0200

    La fonction retourne une liste statique <ul> + 3 <li>. Le Css est commencé , il affiche la list sur la droite de l'ecran

---
+++ frontend/src/components/Eventlist.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Eventlist.css";
+
+export default function Eventlist() {
+  return (
+    <div className="Eventlist">
+      <ul>
+        <li> daaaaaaaaaaaaaaaa </li>
+        <li> Vaaaaaaaaaaa </li>
+        <li> boooooooo </li>
+        <li> couuuuuuuuuuuuu </li>
+      </ul>
+    </div>
+  );
+}
