1245b5953fcc866a9c4c408bae7d165f3227adf5
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-04-19 16:30:03 +0200

    Imports modifiés

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import sunImage from "./assets/sun.png";
 import cloudImage from "./assets/cloud.png";
