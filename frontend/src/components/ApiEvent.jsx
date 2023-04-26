e2f0a5af632323ff54b8d760e4e942cae3e5ee8b
zeitounmax <mthiry1@gmail.com>
2023-04-26 15:26:34 +0200

    first commit

--- frontend/src/components/ApiEvent.jsx
+++ frontend/src/components/ApiEvent.jsx
@@ -20,7 +20,11 @@ function ApiEvent() {
       </button>
       {events &&
         events.records.map((event) => (
-          <p key={event.record.id}>{event.record.fields.title_fr}</p>
+          <p key={event.record.id}>
+            {event.record.fields.title_fr} (lon:
+            {event.record.fields.location_coordinates.lon}/ lat:
+            {event.record.fields.location_coordinates.lat})
+          </p>
         ))}
     </div>
   );
