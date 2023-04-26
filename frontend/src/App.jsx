22690de4db181b270c637423c5a9c437f214b85b
Mohamed4900 <Mohamedibnlakhdar10@gmail.com>
2023-04-26 10:21:31 +0200

    useEffect (dans app.js) remis dans meteo.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
-import sunImage from "./assets/sun.png";
-import cloudImage from "./assets/cloud.png";
-import rainImage from "./assets/rain.png";
 import Meteo from "./components/Meteo";
 import Map from "./components/Map";
 import Navbar from "./components/Navbar";
@@ -11,35 +8,6 @@ import ApiEvent from "./components/ApiEvent";
 function App() {
   const [fetchedData, setFetchedData] = useState(null);
   const [todaysData, setTodaysData] = useState(null);
-
-  useEffect(() => {
-    const fetchOneTime = () => {
-      fetch(
-        "https://api.open-meteo.com/v1/forecast?latitude=49.27&longitude=4.03&hourly=temperature_2m"
-      )
-        .then((response) => response.json())
-        .then((data) => {
-          // extract current hour and filter the data for the current hour only
-          const currentHour = new Date().getHours();
-          const filteredData = data.hourly.time.filter(
-            (time) => new Date(time).getHours() === currentHour
-          );
-          setFetchedData(data);
-          setTodaysData(filteredData);
-        });
-    };
-    fetchOneTime();
-  }, []);
-
-  const getConditionImage = (temperature) => {
-    if (temperature > 15) {
-      return sunImage;
-    }
-    if (temperature > 10) {
-      return cloudImage;
-    }
-    return rainImage;
-  };
 
   const [events, setEvents] = useState(null);
 
@@ -61,8 +29,9 @@ function App() {
       <div className="AppMeteo">
         <Meteo
           todaysData={todaysData}
-          getConditionImage={getConditionImage}
           fetchedData={fetchedData}
+          setFetchedData={setFetchedData}
+          setTodaysData={setTodaysData}
         />
       </div>
       <div className="Map">
